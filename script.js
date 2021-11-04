4bfbf2567ac76d1ae3f4895d2ae6e52726c5d952
perkinswillMIN <93403715+perkinswillMIN@users.noreply.github.com>
2021-11-04 13:39:13 -0500

    nav on all pages

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function navToggle() {
   var navIcon = document.querySelector('#menu-icon');
   navIcon.classList.toggle('fa-times');
   navIcon.classList.toggle('fa-bars');
+  var overflowBody = document.body;
+  overflowBody.classList.toggle('no-overflow')
 }
 
 document.getElementById("menu-icon").onclick = navToggle;
